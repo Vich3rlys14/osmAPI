6281976e45d0d26242e96f3d70ee5f220167a04c
vich3rlys14 <vich3rlys@gmail.com>
2021-12-08 09:31:35 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import fastapi
+from geocoder import osm
+from pydantic import BaseModel
+
+app = fastapi.FastAPI()
+
+class Data(BaseModel):
+    street_name : str
+
+@app.get("/search")
+def search(data : Data):
+    res =  osm(data.street_name)
+    if res.json != None:
+    	return { "data" : res.json , "noResult" : False}
+    return { "noResult" : True} 
