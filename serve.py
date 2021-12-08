bb03acba88a38efaf5ec47af5efa135a0c0272c9
vich3rlys14 <vich3rlys@gmail.com>
2021-12-08 09:49:43 +0300

    host binding

--- serve.py
+++ serve.py
@@ -1,4 +1,5 @@
 import uvicorn
 PORT = 7000
+HOST = "0.0.0.0"
 if "__main__" == __name__ :
-	uvicorn.run ("main:app" , port = PORT , reload=True)
+	uvicorn.run ("main:app" , port = PORT , reload=True , host=HOST)
